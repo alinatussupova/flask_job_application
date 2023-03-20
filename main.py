aa5f3893b775eab43c30e452b97fcc1c5be9e6f3
Alina <alinatussupova@gmail.com>
2023-03-20 14:21:39 +0600

    Connecting Backend To HTML

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 app.run(debug=True, port=5003)
