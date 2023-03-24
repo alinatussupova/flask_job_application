6769c2602fcbfe4f474c7f02b332a6d2b6963939
Alina <alinatussupova@gmail.com>
2023-03-24 14:25:27 +0600

    Submission Notification

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from datetime import datetime
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 import os
 from dotenv import load_dotenv
 
@@ -7,6 +9,8 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
 
 
 app = Flask(__name__)
@@ -15,7 +19,17 @@ app = Flask(__name__)
 # Specify the parameters of the database
 app.config["SECRET_KEY"] = SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = MAIL_USERNAME
+app.config["MAIL_PASSWORD"] = MAIL_PASSWORD
+
+
 db = SQLAlchemy(app)
+
+
+mail = Mail(app)
 
 
 # Creating the database model
@@ -27,8 +41,8 @@ class Form(db.Model):
     email = db.Column(db.String(80))
     date = db.Column(db.Date)
     status = db.Column(db.String(80))  
-    
 
+    
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -36,7 +50,15 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
+        date_object = datetime.strptime(date, "%Y-%m-%d")
         status = request.form["status"]
+
+        # Send the data to the database table
+        form = Form(first_name=first_name, last_name=last_name,
+                    email=email, date=date_object, status=status)
+        db.session.add(form)
+        db.session.commit()
+        flash(f"{first_name}, your form was submitted successfully!", "success")
 
     return render_template("index.html")
 
